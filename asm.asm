16608be2752ee82a4bbeaa9d8fd2684f18f8b9d7
Blairi <axel.avi.fer@gmail.com>
2023-12-05 18:53:59 -0600

    asm example

--- asm.asm
+++ asm.asm
@@ -12,3 +12,20 @@ ja 10100, hreg
 jae 10101, hreg
 jb 10110, hreg
 jbe 10111, hreg
+jmp 11000
+10010:
+move creg, #0
+10011:
+sub ereg, dreg, creg
+10100:
+add dreg, areg, breg
+10101:
+add dreg, areg, breg
+10110:
+add dreg, areg, breg
+sub ereg, dreg, creg
+10111:
+move breg, #5
+move creg, #0
+11000:  
+move breg, #5
